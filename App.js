533af719d86147770d49e6ece1335d2a58bf0fd3
Abinayasree04 <abinayasreej04@gmail.com>
2025-02-23 17:24:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My Vercel Deployed React App 🚀</h1>
+      <p>Deploying React apps on Vercel is super easy!</p>
+    </div>
+  );
+}
+
+export default App;
